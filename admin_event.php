c7eccd4cd41c5835ac205c155f4a2e8764d3c94b
Azri Azmi <azri041@gmail.com>
2021-02-04 07:31:33 +0800

    updated event table

--- admin_event.php
+++ admin_event.php
@@ -29,25 +29,25 @@
       <div class="flex flex-col flex-1 w-full">
 
         <main class="h-full pb-16 overflow-y-auto">
-          <div class="container grid px-6 mx-auto">
+        <div class="mt-20 container grid px-6 mx-auto rounded-lg shadow-xl" style="background-color: #007CBA;">
             <h2
-              class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200"
+              class="my-6 text-2xl font-semibold text-white"
             >
               Manage Events
             </h2>
 
             <!-- With actions -->
             <h4
-              class="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300"
+              class="mb-4 text-lg font-semibold text-white"
             >
               Table with actions
             </h4>
-            <div class="w-full overflow-hidden rounded-lg shadow-xs">
+            <div class="w-full overflow-hidden rounded-lg shadow-lg">
               <div class="w-full overflow-x-auto">
                 <table class="w-full whitespace-no-wrap">
                   <thead>
                     <tr
-                      class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800"
+                      class="text-xs font-semibold tracking-wide text-left text-white uppercase border-b bg-gray-500"
                     >
                       <th class="px-4 py-3">Events</th>
                       <th class="px-4 py-3">Start Date</th>
@@ -166,7 +166,8 @@
                 </script>
                 <form action="event_add_form.php" method="post">
                 <button
-                  class="flex items-center justify-between px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
+                  class="mt-20 flex items-center justify-between px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 
+                  bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
                 >
                 <svg class="w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
--- user_event.php
+++ user_event.php
@@ -22,61 +22,85 @@
       <div class="flex flex-col flex-1 w-full">
 
         <main class="h-full pb-16 overflow-y-auto">
-          <div class="container grid px-6 mx-auto">
+          <div class="mt-20 container grid px-6 mx-auto rounded-lg shadow-xl" style="background-color: #007CBA;">
             <h2
-              class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200"
+              class="my-6 text-2xl font-semibold text-white"
             >
-              View Event
+              View Events
             </h2>
 
 
             <!-- Table user -->
             <h4
-              class="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300"
+              class="mb-4 text-lg font-semibold text-white"
             >
-              Table user
+              Available Events
             </h4>
-            <div class="w-full mb-8 overflow-hidden rounded-lg shadow-xs">
+            <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
               <div class="w-full overflow-x-auto">
                 <table class="w-full whitespace-no-wrap">
                   <thead>
                     <tr
-                      class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800"
+                      class="text-xs font-semibold tracking-wide text-left text-white uppercase border-b bg-gray-500"
                     >
                       <th class="px-4 py-3">Events</th>
-                      <th class="px-4 py-3">Start</th>
-                      <th class="px-4 py-3">End</th>
+                      <th class="px-4 py-3">Start Date</th>
+                      <th class="px-4 py-3">End Date</th>
                       <th class="px-4 py-3">Status</th>
                       <th class="px-4 py-3">Action</th>
                     </tr>
                   </thead>
                   <tbody
-                    class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800"
+                    class="bg-white divide-y"
                   >
-                    <tr class="text-gray-700 dark:text-gray-400">
+                    <tr class="text-gray-700">
                       <td class="px-4 py-3">
                         <div class="flex items-center text-sm">
 
                           <div>
-                            <p class="font-semibold">Hans Burger</p>
-                            <p class="text-xs text-gray-600 dark:text-gray-400">
-                              10x Developer
-                            </p>
+                            <p class="font-semibold">Hack 2 Hire</p>
                           </div>
                         </div>
                       </td>
                       <td class="px-4 py-3 text-sm">
-                        $ 863.45
+                        3/2/2021
+                      </td>
+                      <td class="px-4 py-3 text-sm">
+                        4/2/2021
                       </td>
                       <td class="px-4 py-3 text-xs">
-                        <span
-                          class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100"
+                      <span
+                          class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full"
                         >
                           On Going
                         </span>
                       </td>
+                      <td class="px-4 py-3 text-sm text-green-500 font-semibold">
+                        <a href="" class="btn text-white cursor-default">Enroll</a>
+                      </td>
+                    </tr>
+
+                    <tr class="text-gray-700">
+                      <td class="px-4 py-3">
+                        <div class="flex items-center text-sm">
+
+                          <div>
+                            <p class="font-semibold">Volunteer Event</p>
+                          </div>
+                        </div>
+                      </td>
                       <td class="px-4 py-3 text-sm">
-                        6/10/2020
+                        5/2/2021
+                      </td>
+                      <td class="px-4 py-3 text-sm">
+                        5/2/2021
+                      </td>
+                      <td class="px-4 py-3 text-xs">
+                      <span
+                          class="px-2 py-1 font-semibold leading-tight text-green-700 bg-orange-100 rounded-full"
+                        >
+                          Up Coming
+                        </span>
                       </td>
                       <td class="px-4 py-3 text-sm text-green-500 font-semibold">
                         <a href="" class="btn">Enroll</a>
