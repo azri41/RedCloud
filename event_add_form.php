a2d5c0bcb94668b5381572c2132864b7cd9d15af
NaimSyahmi <calverio.2@gmail.com>
2021-02-04 02:02:47 +0800

    some

--- event_add_form.php
+++ event_add_form.php
@@ -20,6 +20,7 @@
   </head>
   <body>
         <main class="h-full pb-16 overflow-y-auto">
+        <form>
           <div class="container px-6 mx-auto grid">
             <h2
               class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200"
@@ -40,7 +41,7 @@
                 <span class="text-gray-700 dark:text-gray-400">Event Name</span>
                 <input
                   class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
-                  placeholder="Jane Doe"
+                  placeholder="Event Name"
                 />
               </label>
 
@@ -77,14 +78,18 @@
                     <input
                       type="radio"
                       class="text-purple-600 form-radio focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray"
-                      name="trainingType"
+                      name="eventType"
                       value="training"
                     />
                     <span class="ml-2">Training</span>
                   </label>
                 </div>
               </div>
-
+              <span class="ml-2">Start Date: </span>
+              <input type="date" name="eventDateStart">
+              <br>
+              <span class="ml-2">End Date: </span>
+              <input type="date" name="eventDateEnd">
               <label class="block mt-4 text-sm">
                 <span class="text-gray-700 dark:text-gray-400">
                   Quota
@@ -98,8 +103,18 @@
                   <option>5000</option>
                 </select>
               </label>
+              <br>
+              <button
+                  class="flex items-center justify-between px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
+                >
+                <svg class="w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+                </svg>
+                  <span>Add Event</span>
+                </button>
 
             </div>
+            
 
         
           
